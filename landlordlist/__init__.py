8ec6be45a94734374c4c804e4cdb67ab777d365a
hallcode <-Fullwell33->
2020-10-01 23:14:33 +0100

    Updated configuration loading

--- landlordlist/__init__.py
+++ landlordlist/__init__.py
@@ -17,17 +17,13 @@ limiter = Limiter(key_func=get_remote_address)
 
 
 # Flask App
-def create_app(test_config=None):
+def create_app():
     app = Flask(__name__, instance_relative_config=True)
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile(os.path.join(app.instance_path, 'config.py'), silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
+    # load the instance config, if it exists, when not testing
+    app.config.from_pyfile(os.path.join(app.instance_path, 'config.py'), silent=True)
 
-        # ensure the instance folder exists
+    # ensure the instance folder exists
     try:
         os.makedirs(app.instance_path)
     except OSError:
